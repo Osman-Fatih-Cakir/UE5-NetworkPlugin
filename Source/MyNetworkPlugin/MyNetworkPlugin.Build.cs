5603820ad4274e1779175a80dc034e28ffefe37e
Osman Fatih Çakır <osman@otyazilim.com>
2024-07-21 16:19:29 +0300

    Steam Online Subsystem

--- Source/MyNetworkPlugin/MyNetworkPlugin.Build.cs
+++ Source/MyNetworkPlugin/MyNetworkPlugin.Build.cs
@@ -8,6 +8,7 @@ public class MyNetworkPlugin : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"
+		, "OnlineSubsystemSteam", "OnlineSubsystem"});
 	}
 }
